3a40b0919be4277cf71d9136dd5858a29ebe46dd
Juliana Chaves Palm <julianachavespalm@gmail.com>
2023-12-19 16:07:03 -0300

    Alteração de: inimigo, sons, botão de iniciar jogo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,1 +1,74 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".panel__square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+    values: {
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        curretTime: 60,
+    },
+    actions: {
+        timerId: null,
+        countDownTimerId: null,
+        gameStarted: false,
+    },
+};
 
+function countDown() {
+    state.values.curretTime--;
+    state.view.timeLeft.textContent = state.values.curretTime;
+
+    if (state.values.curretTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over! O seu resultado foi: " + state.values.result);
+    }
+}
+
+function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
+}
+
+function playSound(audioName) {
+    let audio = new Audio(`./src/audios/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+}
+
+function addListenerHitBox() {
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if (square.id === state.values.hitPosition) {
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound("hit");
+            }
+        });
+    });
+}
+
+function init() {
+    addListenerHitBox();
+}
+
+function startGame() {
+    if (!state.actions.gameStarted) {
+        state.actions.gameStarted = true;
+        document.getElementById('initButton').disabled = true;
+        state.actions.timerId = setInterval(randomSquare, 1000);
+        state.actions.countDownTimerId = setInterval(countDown, 1000);
+        init();
+    }
+}
